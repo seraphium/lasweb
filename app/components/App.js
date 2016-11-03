a1e6511e1f5bd85787529aa45ed82f46ec24d369
seraphium <viperking@163.com>
2016-11-03 14:50:52 +0800

    first commit on bc3000c web based on existing sample project

---
+++ app/components/App.js
@@ -0,0 +1,20 @@
+/**
+ * Created by jackiezhang on 2016/10/30.
+ */
+import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar history={this.props.history} />
+        {this.props.children}
+        <Footer />
+        </div>
+        );
+    }
+}
+
+export default App;
